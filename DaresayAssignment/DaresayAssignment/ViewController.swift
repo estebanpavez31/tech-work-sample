92216350ffcfab712b0abdd7cabf5554dd6045e1
Esteban Pavez <esteban.pavez.a@gmail.com>
2020-02-10 21:50:25 +0100

    Creates base project with base folders

---
+++ DaresayAssignment/DaresayAssignment/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  DaresayAssignment
+//
+//  Created by Esteban Pavez on 2020-02-10.
+//  Copyright © 2020 Esteban Pavez. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
